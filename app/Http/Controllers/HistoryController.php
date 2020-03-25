bf1f56f77565350f4f7e7d750eb564ca2f6f5644
Phutthichod <pin2017create@outlook.com>
2020-03-25 19:09:23 +0700

    before num

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -18,7 +18,7 @@ class HistoryController extends Controller
         {
             $btnDetailSub = createButton('btn btn-info btn-detailSub','','data-toggle="modal" data-target="#detailSub"','รายละเอียด');
             $tbAll[]=[$borrowList['id'],$borrowList->user->firstName,$borrowList->user->lastName,$borrowList['status'],$borrowList['date_borrow'],[$btnDetailSub,'text-align:center;']];
-            
+
         }
         return view("history",array("tb"=>$tbAll));
 
